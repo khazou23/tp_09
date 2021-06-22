b6e4894330768f3d608862f3da2c43dfe1475bf1
khazou <sandrine.reby@hotmail.fr>
2021-06-22 13:41:30 +0200

    variable element fini

--- class.ean.php
+++ class.ean.php
@@ -14,12 +14,35 @@ class EAN
         9 => "___X_XX ",
     ];
 
+    private $elementB = [
+        0 => "_X__XXX" ,
+        1 => "_XX__XX" ,
+        2 => "__XX_XX" ,
+        3 => "_X____X" ,
+        4 => "__XXX_X" ,
+        5 => "_XXX__X" ,
+        6 => "____X_X" ,
+        7 => "__X___X" ,
+        8 => "___X__X" ,
+        9 => "__X_XXX" ,
+];
+
     private $elementC = [
-        0 => "_X_X_X",
-        1 => "__X_XX_",
-        2 => "_XX__X_",
-        3 => "___X_X__X",
-        4 => "_",
+        0 => "XXX_X_",
+        1 => "XX__XX_",
+        2 => "XX_XX__",
+        3 => "X___X_",
+        4 => "X_XXX__",
+        5 => "X__XXX_",
+        6 => "X_X____",
+        7 => "X___X__",
+        8 => "X__X____",
+        9 => "XXX_X__",
     ];
+
+    private SgardeNormale =" X_X ";
+    private $gardeCentrale = "_X_X_" ;
 }
+
+
 ?>
