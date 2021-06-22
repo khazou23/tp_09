80139f67102d4a2be2326da92086abde26ef1a65
khazou <sandrine.reby@hotmail.fr>
2021-06-22 11:47:11 +0200

    creation class.ean.php exemple de depart creation array element A

--- class.ean.php
+++ class.ean.php
@@ -4,7 +4,14 @@
     $elementA [
         0=> ___XX_X,
         1 => __XX__X,
-        2=>
+        2=> __X__XX,
+        3=> _XXXX_X ,
+        4=> _X_XXXX ,
+        5=> _XX___X ,
+        6=> _X_XXXX ,
+        7=> _XXX_XX ,
+        8=> _XX_XXX ,
+        9=> ___X_XX ,
         ]
 }
 ?>
